9b9fd8d1c8a36f10dfebf86e1f390610732fbe41
andrey <motor4ik@gmail.com>
2020-09-11 10:14:15 +0200

    wallet favourites

--- src/status_im/ui/screens/wallet/send/styles.cljs
+++ src/status_im/ui/screens/wallet/send/styles.cljs
@@ -1,11 +1,8 @@
 (ns status-im.ui.screens.wallet.send.styles
   (:require [status-im.ui.components.colors :as colors]))
 
-(defn sheet [small-screen?]
-  {:background-color        colors/white
-   :border-top-right-radius 16
-   :border-top-left-radius  16
-   :padding-bottom          (if small-screen? 40 60)})
+(defn sheet []
+  {:flex 1})
 
 (defn header [small-screen?]
   {:flex-direction  :row
